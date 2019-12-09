431d16193e5114bc2d165710f77ad17d8ff322ed
Yugo Nishimura <e195719@ie.u-ryukyu.ac.jp>
2019-12-09 13:33:00 +0900

    ステップ２のcommit

--- src/main/jp/ac/uryukyu/ie/e195719/Main.java
+++ src/main/jp/ac/uryukyu/ie/e195719/Main.java
@@ -2,8 +2,13 @@ package jp.ac.uryukyu.ie.e195719;
 
 public class Main {
     public static void main(String[] args){
+        try{
         String str = null;
         System.out.println(str.length());
+        } catch (NullPointerException e){
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
     }
 }
 
